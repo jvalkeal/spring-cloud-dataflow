c3d4522b24182c1dca7ce0d61e5d9cfc54e4f186
Janne Valkealahti <janne.valkealahti@gmail.com>
2015-11-07 08:42:32 +0000

    stash

--- spring-cloud-dataflow-yarn-build-tests/src/test/java/org/springframework/cloud/dataflow/yarn/buildtests/YarnModuleDeployerIT.java
+++ spring-cloud-dataflow-yarn-build-tests/src/test/java/org/springframework/cloud/dataflow/yarn/buildtests/YarnModuleDeployerIT.java
@@ -16,10 +16,21 @@
 
 package org.springframework.cloud.dataflow.yarn.buildtests;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.Assert.assertThat;
 
+import java.io.File;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.hadoop.yarn.api.records.ApplicationId;
+import org.apache.hadoop.yarn.api.records.YarnApplicationState;
+import org.apache.hadoop.yarn.util.ConverterUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -32,13 +43,17 @@ import org.springframework.cloud.dataflow.core.ModuleDeploymentRequest;
 import org.springframework.cloud.dataflow.module.deployer.ModuleDeployer;
 import org.springframework.cloud.dataflow.module.deployer.yarn.DefaultYarnCloudAppService;
 import org.springframework.cloud.dataflow.module.deployer.yarn.YarnCloudAppService;
+import org.springframework.cloud.dataflow.module.deployer.yarn.YarnCloudAppService.CloudAppInstanceInfo;
 import org.springframework.cloud.dataflow.module.deployer.yarn.YarnCloudAppServiceApplication;
 import org.springframework.cloud.dataflow.module.deployer.yarn.YarnModuleDeployer;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.Resource;
+import org.springframework.util.StringUtils;
 import org.springframework.yarn.test.context.MiniYarnClusterTest;
+import org.springframework.yarn.test.support.ContainerLogUtils;
 
 /**
  * Integration tests for {@link YarnCloudAppServiceApplication}.
@@ -75,6 +90,7 @@ public class YarnModuleDeployerIT  extends AbstractCliBootYarnClusterTests {
 		assertThat(context.containsBean("processModuleDeployer"), is(true));
 		assertThat(context.getBean("processModuleDeployer"), instanceOf(YarnModuleDeployer.class));
 		ModuleDeployer deployer = context.getBean("processModuleDeployer", ModuleDeployer.class);
+		YarnCloudAppService yarnCloudAppService = context.getBean(YarnCloudAppService.class);
 	
 		ModuleDefinition timeDefinition = new ModuleDefinition.Builder()
 				.setGroup("ticktock")
@@ -100,17 +116,85 @@ public class YarnModuleDeployerIT  extends AbstractCliBootYarnClusterTests {
 				.build();
 		ModuleDeploymentRequest time = new ModuleDeploymentRequest(timeDefinition, timeCoordinates);
 		ModuleDeploymentRequest log = new ModuleDeploymentRequest(logDefinition, logCoordinates);
+	
 		ModuleDeploymentId timeId = deployer.deploy(time);
-		ModuleDeploymentId logId = deployer.deploy(log);
+		ApplicationId applicationId = assertWaitApp(2, TimeUnit.MINUTES, yarnCloudAppService);
+		File timeStdoutFile = assertWaitFileContent(2, TimeUnit.MINUTES, applicationId, "Started TimeSourceApplication");
 		
-		Thread.sleep(200000);
+		ModuleDeploymentId logId = deployer.deploy(log);
+		File logStdoutFile = assertWaitFileContent(2, TimeUnit.MINUTES, applicationId, "Started LogSinkApplication");
 		
 		deployer.undeploy(timeId);
-		Thread.sleep(20000);
+		timeStdoutFile = assertWaitFileContent(2, TimeUnit.MINUTES, applicationId, "stopped outbound.ticktock.0");
+		
 		deployer.undeploy(logId);
-		Thread.sleep(20000);
+		logStdoutFile = assertWaitFileContent(2, TimeUnit.MINUTES, applicationId, "stopped inbound.ticktock.0");
+		
+		Collection<CloudAppInstanceInfo> instances = yarnCloudAppService.getInstances();
+		assertThat(instances.size(), is(1));
+		
+		List<Resource> resources = ContainerLogUtils.queryContainerLogs(
+				getYarnCluster(), applicationId);
+		
+		assertThat(resources, notNullValue());
+		assertThat(resources.size(), is(6));
+		
+		for (Resource res : resources) {
+			File file = res.getFile();
+			String content = ContainerLogUtils.getFileContent(file);
+			if (file.getName().endsWith("stdout")) {
+				assertThat(file.length(), greaterThan(0l));
+			} else if (file.getName().endsWith("stderr")) {
+				assertThat("stderr with content: " + content, file.length(), is(0l));
+			}
+		}
+	}
+	
+	private File assertWaitFileContent(long timeout, TimeUnit unit, ApplicationId applicationId, String search) throws Exception {
+		File file = null;
+
+		long end = System.currentTimeMillis() + unit.toMillis(timeout);
+		done:
+		do {
+
+			List<Resource> resources = ContainerLogUtils.queryContainerLogs(
+					getYarnCluster(), applicationId);
+			for (Resource res : resources) {
+				File f = res.getFile();
+				String content = ContainerLogUtils.getFileContent(f);
+				if (content.contains(search)) {
+					file = f;
+					break done;
+				}
+			}
+			
+			Thread.sleep(1000);
+		} while (System.currentTimeMillis() < end);
 		
 		
+		assertThat(file, notNullValue());
+		return file;
+	}
+	
+	private ApplicationId assertWaitApp(long timeout, TimeUnit unit, YarnCloudAppService yarnCloudAppService) throws Exception {
+		ApplicationId applicationId = null;
+		Collection<CloudAppInstanceInfo> instances;
+		long end = System.currentTimeMillis() + unit.toMillis(timeout);
+
+		do {
+			instances = yarnCloudAppService.getInstances();
+			if (instances.size() == 1) {
+				CloudAppInstanceInfo cloudAppInstanceInfo = instances.iterator().next();
+				if (StringUtils.hasText(cloudAppInstanceInfo.getAddress())) {
+					applicationId = ConverterUtils.toApplicationId(cloudAppInstanceInfo.getApplicationId());
+					break;
+				}
+			}
+			Thread.sleep(1000);
+		} while (System.currentTimeMillis() < end);
+		
+		assertThat(applicationId, notNullValue());
+		return applicationId;
 	}
 	
 	@Configuration
