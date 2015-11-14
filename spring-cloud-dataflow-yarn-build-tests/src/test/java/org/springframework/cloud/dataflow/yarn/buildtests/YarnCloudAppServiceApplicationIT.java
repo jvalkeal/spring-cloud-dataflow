008b3c7830038027f9d940b00fde9e7f6a8fcdc4
Janne Valkealahti <janne.valkealahti@gmail.com>
2015-11-14 07:34:09 +0000

    stash

--- spring-cloud-dataflow-yarn-build-tests/src/test/java/org/springframework/cloud/dataflow/yarn/buildtests/YarnCloudAppServiceApplicationIT.java
+++ spring-cloud-dataflow-yarn-build-tests/src/test/java/org/springframework/cloud/dataflow/yarn/buildtests/YarnCloudAppServiceApplicationIT.java
@@ -49,7 +49,7 @@ import org.springframework.yarn.test.junit.ApplicationInfo;
 public class YarnCloudAppServiceApplicationIT extends AbstractCliBootYarnClusterTests {
 
 	@Test
-	public void testStream() throws Exception {
+	public void testStream1() throws Exception {
 		Properties instanceProperties = new Properties();
 		instanceProperties.setProperty("spring.yarn.applicationVersion", "app");
 		instanceProperties.setProperty("spring.cloud.dataflow.yarn.version", getProjectVersion());
@@ -96,4 +96,56 @@ public class YarnCloudAppServiceApplicationIT extends AbstractCliBootYarnCluster
 		app.destroy();
 	}
 
+	@Test
+	public void testStream2() throws Exception {
+		String fsUri = getConfiguration().get("fs.defaultFS");
+		Properties instanceProperties = new Properties();
+		instanceProperties.setProperty("spring.yarn.applicationVersion", "app");
+		instanceProperties.setProperty("spring.cloud.dataflow.yarn.version", getProjectVersion());
+		instanceProperties.setProperty("spring.hadoop.fsUri", fsUri);
+		ApplicationContextInitializer<?>[] initializers = new ApplicationContextInitializer<?>[] {
+				new HadoopConfigurationInjectingInitializer(getConfiguration()) };
+		YarnCloudAppServiceApplication app = new YarnCloudAppServiceApplication("app", getProjectVersion(),
+				"application.properties", instanceProperties, null, initializers);
+		
+		app.afterPropertiesSet();
+		setYarnClient(app.getContext().getBean(YarnClient.class));
+				
+		app.pushApplication("app");
+		Collection<CloudAppInfo> pushedApplications = app.getPushedApplications();
+		assertThat(pushedApplications.size(), is(1));
+		
+		String appId = app.submitApplication("app");
+		ApplicationId applicationId = ConverterUtils.toApplicationId(appId);
+		ApplicationInfo info = waitState(applicationId, 60, TimeUnit.SECONDS, YarnApplicationState.RUNNING);		
+		assertThat(info.getYarnApplicationState(), is(YarnApplicationState.RUNNING));
+		
+		Collection<CloudAppInstanceInfo> submittedApplications = app.getSubmittedApplications();
+		assertThat(submittedApplications.size(), is(1));
+		
+		Collection<String> clustersInfo = app.getClustersInfo(applicationId);
+		assertThat(clustersInfo.size(), is(0));
+
+		// before SHDP-532 gets in, create/start individually
+		Map<String, Object> extraProperties1 = new HashMap<String, Object>();
+		extraProperties1.put("containerModules", "org.springframework.cloud.stream.module:hdfs-sink:jar:exec:1.0.0.BUILD-SNAPSHOT");		
+		extraProperties1.put("containerArg1", "spring.cloud.stream.bindings.input.destination=ticktock.0");		
+		app.createCluster(applicationId, "ticktock:hdfs", "module-template", "default", 1, null, null, null, extraProperties1);		
+		
+		app.startCluster(applicationId, "ticktock:hdfs");
+		assertWaitFileContent(2, TimeUnit.MINUTES, applicationId, "Started HdfsSinkApplication");
+		
+		Map<String, Object> extraProperties2 = new HashMap<String, Object>();
+		extraProperties2.put("containerModules", "org.springframework.cloud.stream.module:time-source:jar:exec:1.0.0.BUILD-SNAPSHOT");		
+		extraProperties2.put("containerArg1", "spring.cloud.stream.bindings.output.destination=ticktock.0");		
+		app.createCluster(applicationId, "ticktock:time", "module-template", "default", 1, null, null, null, extraProperties2);
+		
+		app.startCluster(applicationId, "ticktock:time");
+		assertWaitFileContent(2, TimeUnit.MINUTES, applicationId, "Started TimeSourceApplication");
+
+		waitHdfsFile("/tmp/hdfs-sink/data-0.txt", 2, TimeUnit.MINUTES);
+		
+		app.destroy();
+	}
+	
 }
