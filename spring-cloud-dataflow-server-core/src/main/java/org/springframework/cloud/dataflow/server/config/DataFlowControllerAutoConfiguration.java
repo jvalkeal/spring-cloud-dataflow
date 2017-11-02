af424dc12a935ca3e6e3d77fed708ace3e48974b
Janne Valkealahti <janne.valkealahti@gmail.com>
2017-11-02 11:11:00 +0000

    stash

--- spring-cloud-dataflow-server-core/src/main/java/org/springframework/cloud/dataflow/server/config/DataFlowControllerAutoConfiguration.java
+++ spring-cloud-dataflow-server-core/src/main/java/org/springframework/cloud/dataflow/server/config/DataFlowControllerAutoConfiguration.java
@@ -149,8 +149,9 @@ public class DataFlowControllerAutoConfiguration {
 
 		@Bean
 		public SkipperClient skipperClient(SkipperClientProperties properties, RestTemplateBuilder restTemplateBuilder, ObjectMapper objectMapper) {
-			RestTemplate restTemplate = restTemplateBuilder
-					.errorHandler(new SkipperClientResponseErrorHandler(objectMapper)).build();
+			RestTemplate restTemplate = restTemplateBuilder.build();
+//			RestTemplate restTemplate = restTemplateBuilder
+//					.errorHandler(new SkipperClientResponseErrorHandler(objectMapper)).build();
 			return new DefaultSkipperClient(properties.getUri(), restTemplate);
 		}
 
